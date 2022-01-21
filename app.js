6026b3fa3ee03da87d2cf141a1fd6e2d59d9fb3d
rogerio <rogerbenedik@hotmail.com>
2022-01-21 18:31:29 -0400

    feat: protoype add book to list function

---
+++ app.js
@@ -0,0 +1,45 @@
+// book constructor
+function Book (title, author, isbn){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+};
+
+//UI constructor
+function UI () {}
+UI.prototype.addBookToList = function (book){
+    const list = document.getElementById('book-list');
+    // create tr element
+    const row = document.createElement('tr');
+    //insert cols
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+}
+
+
+//event listerners
+document.getElementById('book-form').addEventListener('submit',
+function(e){
+ //get form values
+    const title = document.getElementById('title').value,
+     author = document.getElementById('author').value,
+     isbn = document.getElementById('isbn').value
+
+    //instantiante book
+    const book = new Book(title, author, isbn);
+   
+    // instantiante UI
+    const ui = new UI();
+
+    // add book to list
+    ui.addBookToList(book);
+
+    e.preventDefault();
+});
+
